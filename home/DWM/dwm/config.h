42b82e55b34d54c20018ab427d3142d86c0d4c2a
DanteAKD <marino.kari24@gmail.com>
2024-11-06 11:37:41 +0700

    Volume Script

--- home/DWM/dwm/config.h
+++ home/DWM/dwm/config.h
@@ -4,10 +4,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        // border pixel of windows
 static const unsigned int snap      = 32;       // snap pixel
-static const unsigned int gappih    = 0;        // horiz inner gap between windows
-static const unsigned int gappiv    = 0;        // vert inner gap between windows
-static const unsigned int gappoh    = 0;        // horiz outer gap between windows and screen edge
-static const unsigned int gappov    = 0;        // vert outer gap between windows and screen edge
+static const unsigned int gappih    = 40;        // horiz inner gap between windows
+static const unsigned int gappiv    = 40;        // vert inner gap between windows
+static const unsigned int gappoh    = 40;        // horiz outer gap between windows and screen edge
+static const unsigned int gappov    = 40;        // vert outer gap between windows and screen edge
 static       int smartgaps          = 0;        // 1 means no outer gap when there is only one window
 static const int showbar            = 1;        // 0 means no bar
 static const int topbar             = 1;        // 0 means bottom bar
@@ -79,11 +79,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-c", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accnt, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "st", NULL };
-//static const char *upvol[] = { "sh", "-c", "/home/irisu/DWM-dot/scripts/volume.sh up", NULL };
-//static const char *downvol[] = { "sh", "-c", "/home/irisu/DWM-dot/scripts/volume.sh down", NULL };
-static const char *upvol[]      = { "/usr/bin/wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%+", NULL };
-static const char *downvol[] 	= { "/usr/bin/wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%-", NULL };
-static const char *mutevol[] 	= { "/usr/bin/wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
+static const char *upvol[] = { "sh", "-c", "/home/irisu/DWM-dot/home/DWM/scripts/volume.sh up", NULL };
+static const char *downvol[] = { "sh", "-c", "/home/irisu/DWM-dot/home/DWM/scripts/volume.sh down", NULL };
+static const char *mutevol[] = { "sh", "-c", "/home/irisu/DWM-dot/home/DWM/scripts/volume.sh mute", NULL };
+//static const char *upvol[]      = { "/usr/bin/wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%+", NULL };
+//static const char *downvol[] 	= { "/usr/bin/wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%-", NULL };
+//static const char *mutevol[] 	= { "/usr/bin/wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
 static const char *shutdown[]	= { "shutdown now", NULL};
 static const char *restart[] 	= { "/sbin/reboot", NULL};
 static const char *brightup[]   = { "brightnessctl", "set", "5%+", NULL };
@@ -118,7 +119,7 @@ static const Key keys[] = {
 	//Apps
 		
 	{ MODKEY,			XK_p,		       spawn,	       SHCMD("st -e htop")},				  // sys monitor
-	{ ALTKEY,             XK_f,                  spawn,          SHCMD("brave --new-window") },
+	{ ALTKEY,             XK_f,                  spawn,          SHCMD("firefox --new-window") },
 	{ MODKEY|ShiftMask,             XK_s,                  spawn,          SHCMD("flameshot gui") },                     // screenshot MOD+Shift+S
 	{ MODKEY,                       XK_t,                  spawn,          SHCMD("st -e ranger") },                      // open file manager (Nautilus) MOD+t
 	//{ MODKEY,                       XK_t,                  spawn,          SHCMD("nautilus --new-window") },                      // open file manager (Nautilus) MOD+t
